fe8fabeadbcf09e3f6c8106a44eec8dff1b80874
jocnn <ijann_@outlook.com>
2022-12-07 21:47:16 -0300

    se agregó gett/sett y se refactorizo

--- src/com/bytebank/heredado/ControlBonificacion.java
+++ src/com/bytebank/heredado/ControlBonificacion.java
@@ -3,8 +3,16 @@ package com.bytebank.heredado;
 public class ControlBonificacion {
 	private double suma;
 
+	public double getSuma() {
+		return this.suma;
+	}
+
+	public void setSuma(double suma) {
+		this.suma = suma;
+	}
+
 	public double registrarSalario(Funcionario funcionario) {
-		this.suma = funcionario.getBonificacion() + this.suma;
+		this.suma += funcionario.getBonificacion();
 		System.out.println(this.suma);
 		return this.suma;
 	}
