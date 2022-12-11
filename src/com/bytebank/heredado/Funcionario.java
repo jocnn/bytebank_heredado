8e61ee1a95a66cf93363e985497e284a949b93c2
jocnn <ijann_@outlook.com>
2022-12-11 11:37:15 -0300

    fix tabulación

--- src/com/bytebank/heredado/Funcionario.java
+++ src/com/bytebank/heredado/Funcionario.java
@@ -1,35 +1,33 @@
 package com.bytebank.heredado;
 
 public abstract class Funcionario {
+	private String nombre;
+	private String documentoIdentidad;
+	private double salario;
 
-    private String nombre;
-    private String documentoIdentidad;
-    private double salario;
+	public String getNombre() {
+		return this.nombre;
+	}
 
-    public String getNombre() {
-        return this.nombre;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public String getDocumentoIdentidad() {
+		return this.documentoIdentidad;
+	}
 
-    public String getDocumentoIdentidad() {
-        return this.documentoIdentidad;
-    }
+	public void setDocumentoIdentidad(String documento) {
+		this.documentoIdentidad = documento;
+	}
 
-    public void setDocumentoIdentidad(String documento) {
-        this.documentoIdentidad = documento;
-    }
+	public double getSalario() {
+		return this.salario;
+	}
 
-    public double getSalario() {
-        return this.salario;
-    }
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
 
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
-
-    public abstract double getBonificacion();
-
+	public abstract double getBonificacion();
 }
